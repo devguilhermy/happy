f3d8884a7370ecc50d1f9a74b6bf16de157315c6
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2020-10-14 10:54:36 -0300

    Project started

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css";
+
+export default function Landing() {
+    return (
+        <div id="landing-page">
+            <h1>Alou</h1>
+        </div>
+    );
+}
