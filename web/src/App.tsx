9ba97abcb1a5f3231f01f46e99696cce540faacc
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2020-10-14 13:36:49 -0300

    Landing page finished

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Landing from "./pages/Landing";
 
+import "./styles/global.css";
+
 function App() {
     return <Landing />;
 }
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 
+import { FiArrowRight } from "react-icons/fi";
 import "./styles.css";
+import logoImg from "../../images/logo.svg";
 
 export default function Landing() {
     return (
         <div id="landing-page">
-            <h1>Alou</h1>
+            <div className="content-wrapper">
+                <img src={logoImg} alt="Logo Happy" />
+                <main>
+                    <h1>Leve felicidade para o mundo</h1>
+                    <p>
+                        Visite orfanatos e mude o dia de muitas
+                        crianças
+                    </p>
+                </main>
+                <div className="location">
+                    <strong>Goiânia</strong>
+                    <span>Goiás</span>
+                </div>
+                <a href="" className="enter-app">
+                    <FiArrowRight size={24} color="black" />
+                </a>
+            </div>
         </div>
     );
 }
